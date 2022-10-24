99694da9f82968ab25d07a7dd397131e48168af7
weisrc <weihengjiang64@hotmail.com>
2022-10-23 23:50:19 -0400

    added ci

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: 'fpgaudio',
       // analyze: true,
       // env: {},
       // rawDefine: {}
